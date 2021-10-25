dd7d86755c1c3f224ae677b8215e6a702ba6f238
prabhat <prabhatdbg21@gmail.com>
2021-10-25 13:58:02 +0530

    block-Bjaaal

--- block-BJaaal/code/index.js
+++ block-BJaaal/code/index.js
@@ -4,8 +4,8 @@
 - Define a variable named `username` using `const` keyword and assign your name as a value.
 - Define another variable named `bio` and assign a value `Learning to code`. Use `let` keyword to define the variable.
 */
-const username = Prabhat ;
-let bio = Learning to code ;
+const username = "Prabhat";
+let bio = "Learning to code";
 
 let firstName = "Arya";
 let lastName = "Stark";
@@ -14,15 +14,16 @@ let lastName = "Stark";
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
   - Alert the message `I'm Arya Stark of Winterfell` using the variable name firstName and lastName.
 */
-alert ("I am Prabht and my bio is Learning to code") ;
-alert ("I'm firstNmane lastName of winterfell ");
+alert ("I am " + username + " " + "and my bio is"+ " " + bio);
+alert ("I\'m " + firstName + " " + lastName + " " + "of winterfell");
 
 /* Do the following using string template literal
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
   - Alert the message `I'm Arya Stark of Winterfell` using the variable name firstName and lastName.
 */
-alert {"I am Prabht and my bio is Learning to code"} ;
-alert ("I'm firstNmane lastName of winterfell ");
+alert (`I am ${username} and my bio is ${bio}`) ;
+alert (`I'm ${firstName} ${lastName} of winterfell`);
+
 
 let amount = 200;
 const taxPercent = 10;
@@ -33,8 +34,8 @@ const taxPercent = 10;
   `
   - Above message should be in two lines
 */
-alert "The total amount is = \n ${(amount * taxPercent) / 100 } ";
-
+alert  (`The total amount is ${amount}
+the tax amount is ${(amount * taxPercent) / 100}`);
 
 /* Use double quote and escape character:
   - Alert a message saying `The total amount is [amount]
@@ -42,4 +43,5 @@ alert "The total amount is = \n ${(amount * taxPercent) / 100 } ";
   `
   - Above message should be in two lines
 */
-alert "The total amount is = amount \n the tex amount is ${(amount * taxPercent) / 100} " ;
+
+alert  ("The total amount is" + amount + "\n the tax amount is" + ((amount * taxPercent) / 100));
